eae8007b2497d99b3f249c6b8c9f7856b9bc8799
Francis Hilaire <Prometee@users.noreply.github.com>
2019-08-16 14:27:27 +0300

    Add docker push

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+image="fluxaudio/php-fpm:$VERSION"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag $image $image
 docker push $image
